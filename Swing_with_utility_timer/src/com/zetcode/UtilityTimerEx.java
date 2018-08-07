197d6c98afe373dced8dd38b1c3db05c7eb23ca7
selman birinci <selmanbirinci61@gmail.com>
2018-08-07 13:59:47 -0400

    added swing application with Utility timer

---
+++ Swing_with_utility_timer/src/com/zetcode/UtilityTimerEx.java
@@ -0,0 +1,30 @@
+package com.zetcode;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class UtilityTimerEx extends JFrame{
+	
+	public UtilityTimerEx() {
+		initUI();
+	}
+
+	private void initUI() {
+		add(new Board());
+		
+		setResizable(false);
+		pack();
+		
+		setTitle("star");
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(()->{
+			JFrame ex = new UtilityTimerEx();
+			ex.setVisible(true);
+		});
+	}
+}
